175dcc32b82b058dbdc2bc0da8de6933a27148dd
Muhammad Harris <muhammadhha@gmail.com>
2025-10-11 00:54:23 +0500

    Login and signup Features added

---
+++ app/src/main/java/com/example/smd_assignment_i230796/User.kt
@@ -0,0 +1,9 @@
+package com.example.smd_assignment_i230796
+
+data class User(
+    val username: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val dob: String? = null,
+    val email: String? = null
+)
